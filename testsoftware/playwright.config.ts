32353ba4541e788b750e3a136dc078bc1c39790c
TrtleUnit <113350372+TrtleUnit@users.noreply.github.com>
2026-03-04 13:00:50 +0100

    Update playwright.config.ts

--- testsoftware/playwright.config.ts
+++ testsoftware/playwright.config.ts
@@ -5,7 +5,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig( {
   testDir: './tests/',
-  timeout: 30 * 1000,
+  timeout: 60 * 1000,
   expect: {
     timeout: 25000,
     toHaveScreenshot: { maxDiffPixels: 60 },
